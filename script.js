8b468b01a1e43e4d9840a89d00820d6eac839ce4
annunziatadr99 <anudar99@hotmail.it>
2024-11-13 16:54:56 +0100

    creare gli elementi

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const fetchBook = () => {
-    fetch("https://striveschool-api.herokuapp.com/books")
+const fetchBooks = () => {
+    fetch(" https://striveschool-api.herokuapp.com/books")
+    .then(responseObj => {
+        console.log(responseObj);
+
+        if (responseObj.ok) {
+            return responseObj.json()
+            
+        }
+    })
+
+    .then(responseObj =>{
+        const row = document.getElementById("card-container");
+        console.log(responseObj)
+
+        responseObj.forEach(char => {
+            const col = document.createElement("div");
+            col.className = "col"
+            const img = document.createElement("img");
+            img.src = char.img;
+
+            row.appendChild(col)
+            col.appendChild(img)
+        
+            
+        });
+        
+
+    })
+    .catch(err => console.log(err));
+
     
+}
+window.onload = () => {
+    fetchBooks();
 }
