3e75bbbf8a4e25ce20ef67c2c9f0d98222d60244
annunziatadr99 <anudar99@hotmail.it>
2024-11-14 09:06:41 +0100

    da continuare i bottoni

--- script.js
+++ script.js
@@ -17,13 +17,37 @@ const fetchBooks = () => {
             const col = document.createElement("div");
             col.className = "col"
             const img = document.createElement("img");
-            img.style.width = "80%"
+            img.style.width = "85%"
             img.src = char.img;
+            const titleCard = document.createElement("h4");
+            titleCard.className = "text-light"
+            titleCard.style.width= "85%"
+            titleCard.innerText = char.title;
+            const btnCard = document.createElement("button");
+            btnCard.className = "btn btn-success me-2";
+            btnCard.innerText = "Buy";
+            const btnRemove = document.createElement("button");
+            btnRemove.className = "btn btn-danger";
+            btnRemove.innerText = "Remove";
+            const pPrice = document.createElement("p");
+            pPrice.className = "text-success fs-2"
+            pPrice.innerText = char.price + "$";
+
+
+            
+
+
+
 
             row.appendChild(col)
             col.appendChild(img)
-        
-            
+            col.appendChild(titleCard);
+            col.appendChild(btnCard);
+            col.appendChild(btnRemove)
+            col.appendChild(pPrice)
+            btnRemove.addEventListener ("click", function() {
+                row.removeChild(col)
+            })
         });
         
 
